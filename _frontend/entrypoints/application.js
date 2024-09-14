d4e5f9e3e63c04386d823b12f98584155b658931
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2024-09-14 16:09:53 +0300

    update home page

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -24,16 +24,6 @@ window.auth = {
   check() {
     return Boolean(this.get());
   },
-
-  authUrl() {
-    const params = new URLSearchParams({
-      client_id: import.meta.env.VITE_GITHUB_APP_CLIENT_ID,
-      scope: import.meta.env.VITE_GITHUB_AUTH_SCOPE,
-      redirect_uri: import.meta.env.VITE_AUTH_CALLBACK_URL,
-    });
-
-    return `${import.meta.env.VITE_GITHUB_SITE_URL}/login/oauth/authorize?${params}`;
-  },
 };
 
 window.Alpine = Alpine;
