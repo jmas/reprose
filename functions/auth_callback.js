7f4348c75a66af5990f9daa940d8ee8d63753d89
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2024-09-14 15:35:43 +0300

    update 'auth_callback'

--- functions/auth_callback.js
+++ functions/auth_callback.js
@@ -2,6 +2,19 @@ export async function onRequest({ request, env }) {
   const code = new URL(request.url).searchParams.get("code");
 
   try {
+    return Response.json({
+      method: "POST",
+      headers: {
+        accept: "application/json",
+      },
+      body: JSON.stringify({
+        client_id: env.GITHUB_APP_CLIENT_ID,
+        client_secret: env.GITHUB_APP_CLIENT_SECRET,
+        redirect_uri: env.REDIRECT_URI,
+        code,
+      }),
+    });
+
     const response = await fetch(
       `${env.GITHUB_SITE_URL}/login/oauth/access_token`,
       {
